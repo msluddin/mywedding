fc1e3d38aa70d62173b2d9078ccab61b01a59e37
Salah Uddin <msluddin@gmail.com>
2013-12-05 15:32:42 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mywedding::Application.config.secret_key_base = 'bff93339fee08667ec7345ccdb00551863aff94829c533f8b1794a3d14f0c38e921d1fbaaaf9f4f53779f467cda08742269f9ecb9df4fc1792e73fce8885abce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mywedding::Application.config.session_store :cookie_store, key: '_mywedding_session'
